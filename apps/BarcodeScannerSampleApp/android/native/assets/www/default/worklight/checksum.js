f44987fd2c0e0d7f5057a0ae625caed87e9f686f
Subhodip Kumar <subhodip.kumar@gmail.com>
2014-03-20 01:26:57 +0530

    added base app to repository

---
+++ apps/BarcodeScannerSampleApp/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":352324634,"date":1393417062664,"machine":"Subhodip-Kumars-MacBook-Pro.local"};
+/* Date: Wed Feb 26 17:47:42 GMT+05:30 2014 */
---
+++ apps/BarcodeScannerSampleApp/android/native/src/com/BarcodeScannerSampleApp/GCMIntentService.java
@@ -0,0 +1,5 @@
+package com.BarcodeScannerSampleApp;
+
+public class GCMIntentService extends com.worklight.androidgap.push.GCMIntentService{
+	//Nothing to do here...
+}
